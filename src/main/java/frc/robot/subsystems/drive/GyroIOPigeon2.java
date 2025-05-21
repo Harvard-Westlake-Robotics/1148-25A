53dc883845da421916859ed59a971d46d3416e81
LondonWizard <quinn.harris@icloud.com>
2025-05-20 17:42:24 -0700

    More intake testing

--- src/main/java/frc/robot/subsystems/drive/GyroIOPigeon2.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIOPigeon2.java
@@ -53,7 +53,6 @@ public class GyroIOPigeon2 implements GyroIO {
     inputs.connected = BaseStatusSignal.refreshAll(yaw, yawVelocity).equals(StatusCode.OK);
     inputs.yawPosition = Rotation2d.fromDegrees(yaw.getValueAsDouble());
     inputs.yawVelocityRadPerSec = Units.degreesToRadians(yawVelocity.getValueAsDouble());
-    System.out.println(Units.degreesToRadians(yawVelocity.getValueAsDouble()));
 
     inputs.odometryYawTimestamps =
         yawTimestampQueue.stream().mapToDouble((Double value) -> value).toArray();
